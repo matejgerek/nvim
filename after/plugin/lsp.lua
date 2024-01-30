bd4f50e610af9ab83b201de0ab6c2251b8520ec4
matejgerek <maftvej@gmail.com>
2024-01-30 18:40:05 +0100

    Add prettier to lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,14 +1,36 @@
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.on_attach(function(flient, bufnr)
-	lsp_zero.default_keymaps({buffer = bufnr})
+lsp_zero.on_attach(function(client, bufnr)
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {'tsserver'},
+	ensure_installed = { 'tsserver' },
 	handlers = {
 		lsp_zero.default_setup,
+		require("lspconfig").tsserver.setup({
+			on_init = function(client)
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentFormattingRangeProvider = false
+			end,
+
+		})
 	},
 })
+
+local conform = require("conform")
+conform.setup({
+	formatters_by_ft = {
+		javascript = { { "prettierd" } },
+		typescript = { { "prettierd" } },
+		typescriptreact = { { "prettierd" } },
+		javascriptreact = { { "prettierd" } },
+	},
+	format_on_save = {
+		lsp_fallback = true,
+		timeout_ms = 500
+	},
+})
+vim.keymap.set("n", "<leader>f", conform.format, {})
--- lua/matejgerek/packer.lua
+++ lua/matejgerek/packer.lua
@@ -4,11 +4,11 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		requires = { {'nvim-lua/plenary.nvim'} }
+		requires = { { 'nvim-lua/plenary.nvim' } }
 	}
 	use { "rose-pine/neovim", as = "rose-pine", config = function()
 		vim.cmd('colorscheme rose-pine')
-	end}
+	end }
 
 	use {
 		'nvim-treesitter/nvim-treesitter',
@@ -19,8 +19,8 @@ return require('packer').startup(function(use)
 	use {
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
-		requires = { {"nvim-lua/plenary.nvim"} }
-	}	
+		requires = { { "nvim-lua/plenary.nvim" } }
+	}
 
 	use "mbbill/undotree"
 	use "tpope/vim-fugitive"
@@ -28,15 +28,16 @@ return require('packer').startup(function(use)
 		'VonHeikemen/lsp-zero.nvim',
 		branch = 'v3.x',
 		requires = {
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+			{ 'williamboman/mason.nvim' },
+			{ 'williamboman/mason-lspconfig.nvim' },
 
 			-- LSP Support
-			{'neovim/nvim-lspconfig'},
+			{ 'neovim/nvim-lspconfig' },
 			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
+			{ 'hrsh7th/nvim-cmp' },
+			{ 'hrsh7th/cmp-nvim-lsp' },
+			{ 'L3MON4D3/LuaSnip' },
 		}
 	}
+	use({ "stevearc/conform.nvim" })
 end)
