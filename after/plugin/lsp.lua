87e3cdb2341c9046b92a4aa36f8c0aff154d3d7e
matejgerek <maftvej@gmail.com>
2024-01-30 17:27:37 +0100

    Add lsp basic configuration

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,14 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(flient, bufnr)
+	lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+	ensure_installed = {'tsserver'},
+	handlers = {
+		lsp_zero.default_setup,
+	},
+})
