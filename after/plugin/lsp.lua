bc2768bcb564dbb0c10e1eee5e89534600fd4146
matejgerek <maftvej@gmail.com>
2024-01-30 22:33:17 +0100

    Preselect first item in lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -23,3 +23,16 @@ require('mason-lspconfig').setup({
 		end
 	},
 })
+
+
+local cmp = require("cmp")
+
+cmp.setup({
+	preselect = 'item',
+	completion = {
+		completeopt = 'menu,menuone,noinsert'
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
+	})
+})
