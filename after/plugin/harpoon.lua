7ca4e25ce7f7fbd02dcd96b4495ec93a1e8ead4a
Matej Gerek <matej.gerek@student.tuke.sk>
2024-02-01 22:27:49 +0100

    Change quick toggle

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -7,7 +7,7 @@ harpoon:setup({settings = {
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-ú>", function() harpoon:list():prev() end)
